6f95c2a5b73e8d5f593d686c26b0f9962fc54851
Test <test@test.com>
2026-05-22 02:42:20 +0100

    Add Cloudflare Workers deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    if (process.env.DISABLE_WEBPACK_CACHE === "1") {
+      config.cache = false;
+    }
+
+    return config;
+  },
 }
 
 export default nextConfig
